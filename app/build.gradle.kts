4f083b56d8445e66b59fb23911ff2d8d220e492f
Faruk Gokceoglu <farukg.dev@gmail.com>
2026-01-03 14:00:57 +0100

    feat(app): add splash screen (refs #29) - Also disable dynamic color to keep theme consistent

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.navigation3.ui)
     implementation(libs.androidx.lifecycle.viewmodel.navigation3)
+    implementation(libs.androidx.core.splashscreen)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
